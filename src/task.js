2c5c1dd7e02c051ad3895eac70aa1bc916839ac4
AmaliaGM <mariaamaliagoemzmoro@gmail.com>
2022-07-15 20:12:55 -0300

    fix eslint linters

--- src/task.js
+++ src/task.js
@@ -7,12 +7,12 @@ export default class addItem {
 }
 
 export function printChores() {
-const choreList = document.getElementById('todoList');
-const choresArr = JSON.parse(localStorage.getItem('choresArr'));
+  const choreList = document.getElementById('todoList');
+  const choresArr = JSON.parse(localStorage.getItem('choresArr'));
 
-if (choresArr !== null) {
+  if (choresArr !== null) {
     for (let i = 0; i < choresArr.length; i += 1) {
-    const chores = `<li id='chore${i}'class='chore'>
+      const chores = `<li id='chore${i}'class='chore'>
     <div class='chore-info'>            
     <input type="checkbox" class="todoCheckbox">
         <p class='ID'>${choresArr[i].ID}</p>
@@ -25,29 +25,29 @@ if (choresArr !== null) {
         </button>
     </div>
     </li>`;
-    choreList.innerHTML += chores;
+      choreList.innerHTML += chores;
     }
-}
+  }
 }
 printChores();
 
 export function deleteItem() {
-const element = document.querySelector('#todoList');
-element.addEventListener('click', (e) => {
+  const element = document.querySelector('#todoList');
+  element.addEventListener('click', (e) => {
     if (e.target.className === 'fa-solid fa-trash-can') {
-    const index = parseInt(e.target.parentElement.id, 10);
-    console.log(index);
-    let choresArr = JSON.parse(localStorage.getItem('choresArr'));
-    /* console.log(choresArr); */
-    choresArr = choresArr.filter((chores) => chores.ID !== index);
-    for (let i = 0; i < choresArr.length; i+=1) {
+      const index = parseInt(e.target.parentElement.id, 10);
+      console.log(index);
+      let choresArr = JSON.parse(localStorage.getItem('choresArr'));
+      /* console.log(choresArr); */
+      choresArr = choresArr.filter((chores) => chores.ID !== index);
+      for (let i = 0; i < choresArr.length; i+=1) {
         choresArr[i].ID = i + 1;
+      }
+      localStorage.setItem('choresArr', JSON.stringify(choresArr));
+      // clearChores();
+      printChores();
+      document.location.reload();
     }
-    localStorage.setItem('choresArr', JSON.stringify(choresArr));
-    // clearChores();
-    printChores();
-    document.location.reload();
-    }
-});
+  });
 }
 deleteItem();
