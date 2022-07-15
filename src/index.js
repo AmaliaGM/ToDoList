6c91cb2dfd6a93e129e46b1de24b96d84775a771
AmaliaGM <mariaamaliagoemzmoro@gmail.com>
2022-07-15 17:01:12 -0300

    fixed linters

--- src/index.js
+++ src/index.js
@@ -61,7 +61,6 @@ element.addEventListener('click', (e) => {
     for (let i = 0; i < choresArr.length; i+=1) {
       choresArr[i].ID = i + 1;
     }
-      
     localStorage.setItem('choresArr', JSON.stringify(choresArr));
     // clearChores();
     printChores();
