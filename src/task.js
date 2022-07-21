a5c1a733988020807862cf07ce77639d2437dbf4
AmaliaGM <mariaamaliagoemzmoro@gmail.com>
2022-07-21 18:55:43 -0300

    reset, complete true, error webpack

--- src/task.js
+++ src/task.js
@@ -1,3 +1,5 @@
+import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript";
+
 export default class AddItem {
   constructor(ID, chore, complete = false) {
     this.ID = ID;
@@ -19,7 +21,7 @@ export function printChores() {
         <p class='ID'>${choresArr[i].ID}</p>
         <input type='text' data-index='${choresArr[i].ID}' 
               class='chore' value='${choresArr[i].chore}'/>
-        <p class='complete'>complete</p>
+        <p class='complete' value=false>complete</p>
     </div>
     <div class='buttonCont'>
     <button id="${choresArr[i].ID}" class='remove-btn'>
@@ -33,6 +35,45 @@ export function printChores() {
   }
 }
 // printChores();
+
+let chores = document.querySelector('todoList')
+let li = document.createElement('li')
+li.classList = 'chore'
+let check = document.createElement('input')
+check.setAttribute('type', 'checkbox')
+check.classList = 'check'
+chores.appendChild(check)
+let id = document.createElement('p')
+id.classList = 'ID'
+chores.appendChild(id)
+let chore = document.createElement('input')
+chore.classList = 'chore'
+chores.appendChild(chore)
+let complete = document.createElement('input')
+complete.classList = 'complete'
+chores.appendChild(complete)
+chores.appendChild(li)
+
+for (let i = 0;i< choresArr.length; i++) {
+  if(choresArr !== null){
+    if (check.checked) {
+  complete.value === true;
+    }
+  }
+}
+
+let choresArr = JSON.parse(localStorage.getItem('choresArr'));
+function resetAll() {
+  if(choresArr !== null) {
+  for (let i = 0; i < choresArr.length; i++){
+      choresArr.splice(0, choresArr.length);
+   /*  localStorage.clear(); */
+  }
+  localStorage.setItem('choresArr', JSON.stringify(choresArr));
+}
+}
+let deleteAll = document.getElementById('remove-all');
+deleteAll.addEventListener('click', resetAll());
 
 function updateDescription() {
   const input = document.querySelectorAll('input.chore');
@@ -55,12 +96,6 @@ function updateDescription() {
   );
 }
 
-function deleteItems() {
-  localStorage.clear();
-}
-let removeBut = document.getElementById('remove-all');
-removeBut.addEventListener('click', deleteItems())
-
 const element = document.querySelector('#todoList');
 element.addEventListener('click', (e) => {
   if (e.target.className === 'fa-solid fa-trash-can') {
@@ -73,8 +108,25 @@ element.addEventListener('click', (e) => {
       choresArr[i].ID = i + 1;
     }
     localStorage.setItem('choresArr', JSON.stringify(choresArr));
-    clearChores();
+    // clearChores();
     printChores();
     // document.location.reload();
   }
 });
+
+/* 
+const clrBtn = document.getElementById('remove-all');
+clrBtn.addEventListener('click', () => {
+  for (let i = 0; i < choresArr.length; i += 1) {
+    if (choresArr[i].completed) {
+      choresArr.splice(i, 1);
+      choresArr.forEach((chores) => {
+        if (chores.index > i) {
+          chores.index -= 1;
+        }
+      });
+      i -= 1;
+    }
+  }
+  localStorage.setItem('todoList', JSON.stringify(choresArr));
+}); */
